a1225133702449a2666e8f145908023fd32d401f
shamilvasiyev <vasiyevsamil@gmail.com>
2024-11-22 21:21:59 +0400

    js codes added

---
+++ Lesson-15(JS)/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// console.log(25);
+
+// <!-- -->
+
+/* 
+console.log("This is a first message");
+console.log("This is a second message");
+*/
+
+// console.log("This is a first message");
+// console.log("This is a first message");
+// console.log("This is a first message");
+// console.log("This is a first message");
+// console.log("This is a first message");
+// console.log("This is a first message");
+// console.log("This is a first message");
+
+/*
+var
+
+let
+const
+*/
+
+// const suyunQannamaTempraturu = 100; // number tipidir
+
+// let message;
+// message = "Hello world!"; // string tipidir
+// console.log(message);
+
+// let name = "Shamil";
+// console.log(name);
+
+// let fullName = "Fərid İmanov"; // camelCase
+// let FullName = "Fərid İmanov"; // PascalCase
+// const FULL_NAME = "Fərid İmanov"; // SNAKE_CASE
+
+// global scope
+
+// let age;
+// age = 25;
+
+// console.log(age);
+
+// {
+//   // block scope
+//   let age = 26;
+// }
+
+const userName = prompt("Adinizi daxil edin");
+console.log(userName);
+
+// confirm("Formu tesdiq edirsizmi?");
+
+// console.info("Bu bir xeta mesajidir");
